494650763cab8c9072c9bcef148f3ddd08d3d5c0
Redp <82160772+BasM1517@users.noreply.github.com>
2022-01-18 21:22:06 +0100

    made a api call

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log("hello");
+https://api.instagram.com/oauth/authorize
+  ?client_id=425999945873461
+  &redirect_uri=https://basm1517.github.io/
+  &scope=user_profile,user_media
+  &response_type=code
